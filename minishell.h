cd673deb129e201d42dcb2bf763561cdb5fc3495
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2024-09-09 10:46:38 +0200

    few files created

---
+++ minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 10:45:00 by dpaluszk          #+#    #+#             */
+/*   Updated: 2024/09/09 10:46:11 by dpaluszk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdbool.h>
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+typedef struct minishell_s
+{
+	/* data */
+}				minishell_t;
+
+#endif
