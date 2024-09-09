e3cca054573edad1015ca22226a774f7fad930f6
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2024-09-09 14:11:33 +0200

    header file edited

--- minishell.h
+++ minishell.h
@@ -6,22 +6,23 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 10:45:00 by dpaluszk          #+#    #+#             */
-/*   Updated: 2024/09/09 10:46:11 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2024/09/09 14:09:51 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <stdbool.h>
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <fcntl.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/_types.h>
+# include <unistd.h>
+
 typedef struct minishell_s
 {
 	/* data */
-}				minishell_t;
+}	minishell_t;
 
 #endif
