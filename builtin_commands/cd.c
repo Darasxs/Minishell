d5bf96fc9e7fbb055195024e92ff149b245dd56a
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2024-09-13 17:08:04 +0200

    modified printing prompt function so it prints the username also, and optmized the code

--- builtin_commands/cd.c
+++ builtin_commands/cd.c
@@ -3,31 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 13:54:20 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/09/12 15:03:37 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/09/13 16:28:35 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../minishell.h"
 
 void	cd(minishell_t *line)
 {
-	if (!line->split_commands[1])
+	if (!line->split_commands[1] || line->split_commands[1][0] == '~')
 	{
 		line->env = getenv("HOME");
 		if (!line->env)
 			return ;
 		if (chdir(line->env) != 0)
 			ft_error("Error while finding the Home directory\n", NULL);
-	}
-	else if (line->split_commands[1][0] == '.' &&
-		line->split_commands[1][1] == '.' &&
-		line->split_commands[1][2] == '\0')
-	{
-		if (chdir("..") != 0)
-			ft_error("cd: no such file or directory: ", line->split_commands[1]);
 	}
 	else if (line->split_commands[1])
 	{
--- main.c
+++ main.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 12:33:16 by dpaluszk          #+#    #+#             */
-/*   Updated: 2024/09/12 16:10:11 by paprzyby         ###   ########.fr       */
+/*   Updated: 2024/09/13 17:04:24 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	term_init(minishell_t *line)
-{
-	char	*new_term;
+//void	term_init(minishell_t *line)
+//{
+//	char	*new_term;
 
-	if (tcgetattr(STDIN_FILENO, line->old_term) == -1)
-		ft_error("Error\n", NULL);
-	new_term = ft_getenv("TERM");
-	if (!new_term)
-		ft_error("Error\n", NULL);
+//	if (tcgetattr(STDIN_FILENO, line->old_term) == -1)
+//		ft_error("Error\n", NULL);
+//	new_term = ft_getenv("TERM");
+//	if (!new_term)
+//		ft_error("Error\n", NULL);
 
-}
+//}
 
 void	print_beginning(void)
 {
@@ -37,7 +37,7 @@ int	main(void)
 	if (!line)
 		return (1);
 	//signal(SIGINT, SIG_IGN);
-	term_init(line);
+	//term_init(line);
 	print_beginning();
 	while (1)
 		minishell(line);
@@ -51,3 +51,4 @@ int	main(void)
 //exit
 //pipes
 //>>
+// cd po wcisnieciu taba powinna byc opcja przejscia przez wszystkie opcje
