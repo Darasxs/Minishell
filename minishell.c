631fc9be1740bc75df94bf831b3340a38ec8646e
Darasxs <dariuszpaluszkiewicz113@gmail.com>
2024-09-17 15:02:27 +0200

    implemented pipes into the existing code, but still need to optimize this code, and test it thoroughly

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: daras <daras@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 07:19:57 by paprzyby          #+#    #+#             */
-/*   Updated: 2024/09/17 10:17:06 by daras            ###   ########.fr       */
+/*   Updated: 2024/09/17 15:00:46 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,71 +25,70 @@ void	preparing_execution(minishell_t *line)
 void	execute_command(minishell_t *line, int input_fd, int output_fd)
 {
 	preparing_execution(line);
-	if(line->path)
+	if (line->path)
 	{
-		if(input_fd != STDIN_FILENO)
+		if (input_fd != STDIN_FILENO)
 		{
 			dup2(input_fd, STDIN_FILENO);
 			close(input_fd);
 		}
-		if(output_fd != STDOUT_FILENO)
+		if (output_fd != STDOUT_FILENO)
 		{
 			dup2(output_fd, STDOUT_FILENO);
 			close(output_fd);
 		}
 		if (execve(line->path, line->split_commands, NULL) == -1)
-		{
 			ft_error("Execution failed\n", NULL);
-			free(line->path);
-		}
+		free(line->path);
 	}
-	free(line->path);
 }
 
 void	minishell(minishell_t *line)
 {
-	pid_t	pid;
-	int		fd[2];
-	char	**commands;
-	size_t	i;
+	pid_t pid;
+	int fd[2];
+	int input_fd;
+	char **commands;
+	size_t i = 0;
 
-	i = 0;
+	input_fd = STDIN_FILENO;
 	printing_prompt(line);
 	if (line->input)
 	{
 		add_history(line->input);
 		commands = ft_split(line->input, '|');
-		while(commands[i])
+		while (commands[i])
 		{
 			line->split_commands = ft_split(commands[i], ' ');
-			if(commands[i + 1])
+			if (commands[i + 1] && pipe(fd) == -1)
 			{
-				if(pipe(fd) == -1)
-				{
-					free_split(commands);
-					ft_error("Error occured while creating pipe\n", NULL);
-				}
+				free_split(commands);
+				ft_error("Error occurred while creating pipe\n", NULL);
 			}
-			else
-				fd[1] = STDOUT_FILENO;
 			pid = fork();
-			if (pid == 0)
-			{
-				execute_command(line, STDIN_FILENO, fd[1]);
-				exit(0);	
-			}
-			else if (pid < 0)
+			if (pid < 0)
 			{
 				free_split(line->split_commands);
-				ft_error("Error occured while forking\n", NULL);
+				ft_error("Error occurred while forking\n", NULL);
 			}
-			else if (pid > 0)
+			else if (pid == 0)
+			{
+				if (i > 0)
+					dup2(input_fd, STDIN_FILENO);
+				if (commands[i + 1])
+					dup2(fd[1], STDOUT_FILENO);
+				else
+					fd[1] = STDOUT_FILENO;
+				execute_command(line, STDIN_FILENO, fd[1]);
+				exit(0);
+			}
+			else
 			{
 				wait(NULL);
 				close(fd[1]);
-				if(fd[0] != STDIN_FILENO)
-					close(fd[0]);
-				fd[0] = STDIN_FILENO;
+				if (input_fd != STDIN_FILENO)
+					close(input_fd);
+				input_fd = fd[0];   
 			}
 			free_split(line->split_commands);
 			i++;
